3fd403c46dcf688beda3fefd743db82ccb81f07c
monalisabarikntt <monalisa.barik@global.nttdata.com>
2026-04-22 13:20:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return "Hello from AKS!"
+
+@app.route("/healthz")
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
